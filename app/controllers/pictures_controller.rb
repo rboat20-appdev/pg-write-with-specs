555aa9f1193ab8c0301981597448be8c817cc553
Ross Boatwright <ross.boatwright@kellogg.northwestern.edu>
2019-05-29 18:56:50 +0000

    Update Form Fixed

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -18,4 +18,77 @@ class PicturesController < ApplicationController
 
     render("pic_templates/details.html.erb")
   end
+  
+  def byebye
+    
+    pic_id = params.fetch("id_to_delete")
+  
+    photo = Photo.where({:id => pic_id}).at(0)
+    
+    photo.destroy
+    
+    redirect_to("/popular")
+
+  end   
+  
+  def blank_form  
+    
+    render("pic_templates/new_photo_form.html.erb")
+  
+  end
+  
+  def save_photo_row
+    
+    #  Parameters: {"pic_image"=>"https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png", "pic_caption"=>"adsf", "pic_id"=>"117"}
+    
+    p = Photo.new
+    p.image = params.fetch("pic_image")
+    p.caption = params.fetch("pic_caption")
+    p.owner_id = params.fetch("pic_owner_id")
+    
+    p.save
+    
+    redirect_to("/recent")
+    
+  end  
+  
+  def prepopulated_form
+    
+    edit_photo_id = params.fetch("id_to_update")
+
+    @editpic = Photo.where({ :id => edit_photo_id }).at(0)
+
+    render("pic_templates/editphoto.html.erb")
+  end
+  
+  def update_record
+    
+    update_photo_id = params.fetch("id_to_update")
+    
+    @updatepic = Photo.where({:id => update_photo_id}).at(0)
+    
+    @updatepic.image = params.fetch("pic_image")
+    @updatepic.caption = params.fetch("pic_caption")
+    
+    @updatepic.save
+    
+    redirect_to("/photos/"+@updatepic.id.to_s)
+    
+  end
+  
+  def add_new_fan
+    # do this like save new photo but its for save new like
+     
+    l = Like.new
+    l.fan_id = params.fetch("pic_fan_id")
+    l.photo_id = params.fetch("photo_id")
+    
+    l.save
+    
+    redirect_to("/photos/"+l.photo_id.to_s)
+    
+    #could do  + l.photo_id.to_s or #{l.photo_id} -- this is for embedded string
+    
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,19 @@ Rails.application.routes.draw do
 
   match("/users/:id/discover", { :controller => "users", :action => "discover", :via => "get" })
 
+  match("/delete_photo/:id_to_delete", {:controller => "pictures", :action => "byebye", :via => "get"})
+  
+  match("/new_photo_form", {:controller => "pictures", :action =>"blank_form", :via => "get"})
+
+  match("/create_photo_record", {:controller => "pictures", :action => "save_photo_row", :via => "get"})
+
+  match("/existing_photo_form/:id_to_update", {:controller => "pictures", :action => "prepopulated_form", :via => "get"})
+  
+  match("/update_photo_record/:id_to_update", {:controller => "pictures", :action => "update_record", :via => "get"})
+  
+  match("/create_like_record", {:controller => "pictures", :action => "add_new_fan", :via => "get"})
+
+  
 
   # The routes below are for the ActiveAdmin dashboard located at /admin. You can ignore them.
   devise_for :admin_users, ActiveAdmin::Devise.config
